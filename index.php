a862cf16f0a12e040e584cd742abbc76c90b1486
Fabio Cionini <fabio@todo.to.it>
2015-04-08 01:12:48 +0200

    cleanup

--- index.php
+++ index.php
@@ -6,32 +6,12 @@
  * Time: 16:17
  */
 
-use \Example\Controllers\AddressController;
 use \Example\Core\Router;
 use \Example\Config\Routes;
 
 require_once('SplClassLoader.php');
 $loader = new SplClassLoader('Example', '.');
 $loader->register();
-
-//use Example\Models\Address;
-
-//$address = new Address();
-//$address->name = 'nome';
-//$address->phone = '8765786587';
-//$address->address = 'addr';
-//
-//$address->save();
-
-//$a->create(['name' => 'pippo', 'phone' => '7865876567', 'address' => 'iuwyetiu']);
-//
-//$a->update(1, ['name' => 'aggiornato']);
-//
-//$a->destroy(2);
-//
-//$a->show(3);
-//
-//$a->index();
 
 $router = new Router();
 $router->setup(Routes::$data);
